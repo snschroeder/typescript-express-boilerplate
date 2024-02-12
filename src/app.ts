fabc307af4062de2129854b97b81c88e1e28cb42
sns <scott.schroeder@psaudio.com>
2024-02-12 16:12:19 -0700

    uses dotenv for listening port

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello, world');
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
+  console.log(`Server is listening on port ${port}`);
 });
